b339d435ab0e857b4a50fd38bd7d59770cb7ebd8
Nguyen Anh Quan <anhq46724@gmail.com>
2023-07-14 11:16:06 +0700

    uppload page food

--- script.js
+++ script.js
@@ -118,3 +118,20 @@ document.addEventListener('DOMContentLoaded', function () {
     reviewSection.scrollIntoView({ behavior: 'smooth' });
   });
 });
+
+
+//food
+var swiper = new Swiper(".home-slider", {
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 7500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  loop:true,
+});
+
