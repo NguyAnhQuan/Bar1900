15667281a73751477ee4158fa45648067bf297fb
Nguyen Anh Quan <anhq46724@gmail.com>
2023-07-31 22:08:20 +0700

    up js drink //

--- js/drink.js
+++ js/drink.js
@@ -1,39 +1,39 @@
-// pure image zoom
-const zoomer = document.querySelector(".swiper-slide");
-const wrapImg = document.querySelector(".swiper-slide .image");
-const result = document.querySelector(".swiper-slide .result");
-const size = 3;
+// // pure image zoom
+// const zoomer = document.querySelector(".swiper-slide");
+// const wrapImg = document.querySelector(".swiper-slide .image");
+// const result = document.querySelector(".swiper-slide .result");
+// const size = 3;
 
-wrapImg.addEventListener("mousemove", function (e) {
-result.classList.remove('hide')
+// wrapImg.addEventListener("mousemove", function (e) {
+// result.classList.remove('hide')
 
-const img = wrapImg.querySelector("img");
-let x = (e.offsetX / this.offsetWidth) * 100;
-let y = (e.offsetY / this.offsetHeight) * 100;
+// const img = wrapImg.querySelector("img");
+// let x = (e.offsetX / this.offsetWidth) * 100;
+// let y = (e.offsetY / this.offsetHeight) * 100;
 
-// move result
-let posX = e.pageX - zoomer.offsetLeft;
-let posY = e.pageY - zoomer.offsetTop;
-result.style.cssText = `
-            background-image: url(${img.src});
-            background-size: ${img.width * size}px ${img.height * size}px;
-            background-position : ${x}% ${y}%;
-            left: ${posX - 100}px;
-            top: ${posY - 100}px;
-        `;
-});
+// // move result
+// let posX = e.pageX - zoomer.offsetLeft;
+// let posY = e.pageY - zoomer.offsetTop;
+// result.style.cssText = `
+//             background-image: url(${img.src});
+//             background-size: ${img.width * size}px ${img.height * size}px;
+//             background-position : ${x}% ${y}%;
+//             left: ${posX - 100}px;
+//             top: ${posY - 100}px;
+//         `;
+// });
 
-//autoplay
-var swiper = new Swiper(".home-slider", {
-  spaceBetween: 30,
-  centeredSlides: true,
-  autoplay: {
-    delay: 7500,
-    disableOnInteraction: false,
-  },
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-  loop:true,
-});
+// //autoplay
+// var swiper = new Swiper(".home-slider", {
+//   spaceBetween: 30,
+//   centeredSlides: true,
+//   autoplay: {
+//     delay: 7500,
+//     disableOnInteraction: false,
+//   },
+//   pagination: {
+//     el: ".swiper-pagination",
+//     clickable: true,
+//   },
+//   loop:true,
+// });
